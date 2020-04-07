51897a3bea65100f311484a1e3e4e9914f0c0feb
Jake Sylvestre <jakesyl@gmail.com>
2020-04-07 06:16:09 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 NODE_ID=$(echo $(hostname) | awk '{print substr($0,length,1)}')
 echo "NODE_ID is $NODE_ID"
 
-sed -i "s/REPLICA_NAME/replica$NODE_ID/" /etc/clickhouse-server/config.d/macros.xml
+sed -i "s/REPLICA_NAME/replica$NODE_ID/" /macros.xml
+cp -r /macros.xml /etc/clickhouse-server/config.d/macros.xml
 
 /usr/bin/clickhouse-server --config=${CLICKHOUSE_CONFIG}
