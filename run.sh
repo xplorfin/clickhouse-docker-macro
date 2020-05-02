223732dc1f43a851a435b6603026335ce85ccbe9
Jake Sylvestre <jakesyl@gmail.com>
2020-05-02 01:58:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+1#!/bin/bash
 
 NODE_ID=$(echo $(hostname) | awk '{print substr($0,length,1)}')
 echo "NODE_ID is $NODE_ID"
-
-sed -i "s/REPLICA_NAME/replica$NODE_ID/" /macros.xml
+if test $SUFFIX; then
+        sed -i "s/REPLICA_NAME/replica$NODE_ID-$SUFFIX/" /macros.xml
+else
+        sed -i "s/REPLICA_NAME/replica$NODE_ID/" /macros.xml
+fi
 cp -r /macros.xml /config.d/macros.xml
